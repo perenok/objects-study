c6c22015847d12d65ad84a01292a13c69b2f3945
perenok <perenoke@gmail.com>
2022-01-08 20:20:41 +0900

    refactor: 스스로 자신의 데이터를 책임지도록 리팩터링

--- src/main/java/com/perenok/objects/chapter4/Screening.java
+++ src/main/java/com/perenok/objects/chapter4/Screening.java
@@ -14,6 +14,24 @@ public class Screening {
         this.whenScreened = whenScreened;
     }
 
+    public Money calculateFee(int audienceCount) {
+        switch (movie.getMovieType()) {
+            case AMOUNT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculateAmountDiscountedFee().times(audienceCount);
+                }
+                break;
+            case PERCENT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculatePercentDiscountedFee().times(audienceCount);
+                }
+                break;
+            case NONE_DISCOUNT:
+                return movie.calculateNoneDiscountedFee().times(audienceCount);
+        }
+        return movie.calculateNoneDiscountedFee().times(audienceCount);
+    }
+
     public Movie getMovie() {
         return movie;
     }
